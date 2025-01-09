8cafd33c091e5600db9d076c17d7ec86839d2aa3
Rheno001 <renfredreuben@gmail.com>
2025-01-09 20:08:10 +0100

    update

--- src/components/slider.jsx
+++ src/components/slider.jsx
@@ -6,15 +6,15 @@ const Slider = () => {
                     <div className="sliding-text-box w-[110%] z-[999] relative">
                         <div className="sliding-text flex justify-center items-center font-sigmar gap-[10px] lg:gap-[40px]">
                             {/* Original content */}
-                            <div className="lg:text-[20px] flex justify-center sans items-center gap-2 font-[700] text-accent"><p>💸 Affordable Memberships</p> </div>
-                            <div className="lg:text-[20px] flex justify-center sans items-center gap-2 font-[700] text-accent"><p>🎯 Achieve Your Goals Faster</p> </div>
-                            <div className="lg:text-[20px] flex justify-center sans items-center gap-2 font-[700] text-accent"><p>🏋️ Expert Trainers</p> </div>
+                            <div className="text-[20px] lg:text-[25px] flex justify-center sans items-center gap-2 font-[700] text-accent"><p>💸 Affordable Memberships</p> </div>
+                            <div className="text-[20px] lg:text-[25px] flex justify-center sans items-center gap-2 font-[700] text-accent"><p>🎯 Achieve Your Goals Faster</p> </div>
+                            <div className="text-[20px] lg:text-[25px] flex justify-center sans items-center gap-2 font-[700] text-accent"><p>🏋️ Expert Trainers</p> </div>
 
 
                             {/* Duplicate content */}
-                            <div className="lg:text-[20px] flex justify-center sans items-center gap-2 font-[700] text-accent"><p>🔥 Variety of Classes</p> </div> 
-                            <div className="lg:text-[20px] flex justify-center sans items-center gap-2 font-[700] text-accent"><p>🕒 Flexible Hours</p> </div> 
-                            <div className="lg:text-[20px] flex justify-center sans items-center gap-2 font-[700] text-accent"><p>🌟 Exclusive Amenities</p> </div>
+                            <div className="text-[20px] lg:text-[25px] flex justify-center sans items-center gap-2 font-[700] text-accent"><p>🔥 Variety of Classes</p> </div> 
+                            <div className="text-[20px] lg:text-[25px] flex justify-center sans items-center gap-2 font-[700] text-accent"><p>🕒 Flexible Hours</p> </div> 
+                            <div className="text-[20px] lg:text-[25px] flex justify-center sans items-center gap-2 font-[700] text-accent"><p>🌟 Exclusive Amenities</p> </div>
 
                         </div>
                     </div>
