8a28b6e0fcf1aeddf1d95a5ba1665627870df5d0
Rheno001 <renfredreuben@gmail.com>
2025-01-13 13:06:56 +0100

    update

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,8 @@ import { useState } from 'react'
 import './App.css'
 import Navbar from './components/Navbar.jsx'
 import Home from './pages/home/index.jsx'
+import Client from './pages/client/index.jsx'
+import Instructor from './pages/instructor/index.jsx'
 import Footer from './components/Footer.jsx'
 
 
